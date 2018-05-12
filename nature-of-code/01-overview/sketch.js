db2caa5321e667e4b7fcdd2737f756bce6d585ab
christian <christianlemp@gmail.com>
2018-05-12 12:42:05 -0400

    cells create and move within rectangle

--- nature-of-code/01-overview/sketch.js
+++ nature-of-code/01-overview/sketch.js
@@ -1,86 +1,163 @@
 var w;
 
-function setup() {
-  createCanvas(640,360);
-  d = new Dot();
+function Cell(x, y, r) {
+    // initial x,y variables - get updated in each cycle via the move() function
+    this.x = x;
+    this.y = y;
+
+    // origin x, y variables - stored so each cell "remembers" where it started
+    this.origin_x = x;
+    this.origin_y = y;
+
+    // radius of each new cell
+    this.r = r;
+
+    // speed set for each cell as it moves around the canvas
+    this.speedx = random(-1, 1);
+    this.speedy = random(-1, 1);
+
+    this.show = function() {
+        // How to display the cell for each run
+        // stroke(255);
+        noStroke();
+        fill(26, 229, 209, 50)
+        ellipse(this.x, this.y, this.r * 2);
+
+        // Boundary around the origin
+        fill(255, 10);
+        // ellipse(this.origin_x, this.origin_y, 200);
+        rect(this.origin_x-100, this.origin_y-50, 200, 100);
+    }
+
+    this.clickedcell = function(x, y) {
+        var d = dist(this.x, this.y, x, y);
+        if (d < this.r) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    this.clickedcanvas = function(x, y) {
+        var d = dist(this.x, this.y, x, y);
+        if (d > this.r) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    this.split = function() {
+        var cell = new Cell(random(width), random(height), this.r * 0.5);
+        return cell;
+    }
+
+    // Move function to define how each individual cell moves in the canvas
+    this.move = function() {
+        this.x = this.x + this.speedx;
+        this.y = this.y + this.speedy;
+
+        // this.x = constrain(this.x + this.speedx, 100, 100);
+        // this.y = constrain(this.y + this.speedy, 100, 100);
+        // fill(76);
+        // rect(100, 100, 100, 100);
+
+    }
+
+    this.bounce = function() {
+      // Bounce off edges of canvas window by reversing speed
+        // if (this.x > width || this.x < 0) {
+        //     this.speedx = -this.speedx;
+        // }
+        // if (this.y > height || this.y < 0) {
+        //     this.speedy = -this.speedy;
+        // }
+
+      // Bounce off edges of rectangle
+      if (this.x > this.origin_x + 100 || this.x < this.origin_x - 100) {
+          this.speedx = -this.speedx;
+      }
+      if (this.y > this.origin_y + 50 || this.y < this.origin_y - 50) {
+          this.speedy = -this.speedy;
+      }
+
+    }
+
+
+    this.eats = function(other) {
+        var d = dist(this.x, this.y, other.x, other.y);
+        if (d < this.r + other.r) {
+            this.r += other.r * 0.6;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
+
+var cells = [];
+
+function setup() {
+    createCanvas(600, 600);
+    // Initialize "cell" array.
+    // Number of cells doubles with each click. For example:
+    // Start with 1 cell. First mouse click, 2 cells. Next mouse click, 4 cells.
+    // Then 8 cells, Then 16, and so on.
+    // cells.length function is y = 2x; where y = number of cells after Then
+    // mouse click, x is current number of cells
+    var init_num_cells = 1;
+    for (var i = 0; i < init_num_cells; i++) {
+        cells[i] = new Cell(random(width), random(height), 10);
+    }
+}
+
+// Split arrays of cells when a cell is clicked
+// this function doesn't work yet, needs fixing
+// refer to documentation here: https://p5js.org/reference/#/p5.Element/mousePressed
+// function mousePressed() {
+//     for (var i = 0; i < cells.length; i++) {
+//         if (cells[i].clickedcell(mouseX, mouseY)) {
+//             cells.push(cells[i].split());
+//             cells.push(cells[i].split());
+//             cells.splice(i, 1);
+//         }
+//     }
+// }
+
+// Create new cells when mouse is pressed on canvas
+function mousePressed() {
+    cell_radius = random(1,10);
+    for (var i = 0; i < cells.length; i++) { // create the number of new cells that are currently in the system
+        if (cells[i].clickedcanvas(mouseX, mouseY)) {
+            cells.push(new Cell(mouseX, mouseY, cell_radius));
+        }
+    }
+    // fill(255);
+    // cells.push(ellipse(mouseX, mouseY, 100, 100));
+}
+
 
 function draw() {
-  background(51);
-  d.display();
-  // w.update()
-}
+    background(51);
+    for (var i = cells.length - 1; i >= 0; i--) {
+        cells[i].show();
+        cells[i].move();
+        cells[i].bounce();
 
-function Dot() {
-  // this.x = width/2;
-  // this.y = height/2;
-  this.x = mouseClicked() {
-    var x = mouseX;
-  };
-  this.y = mouseClicked();
-
-  this.pos = createVector(100,100);
-  this.vel = createVector(0,0);
-
-// // acceleration can be set as a static variable as well
-// // to change position with each cycle, rotate the acceleration angle
-// // in the update function
-//   this.acc = p5.Vector.fromAngle(random(TWO_PI));
-//   this.acc.setMag(0.001);
-//
-//   this.update = function() {
-//     // store mouse position
-//     var mouse = createVector(mouseX, mouseY);
-// // Acceleration
-//     // change acceleration randomly
-//     // this.acc = createVector(random(-1,1),random(-1,1));
-//
-//     // acceleration to follow mouse location
-//     // this.acc = p5.Vector.sub(mouse,this.pos);
-//
-//     // random acceleration
-//     // method #1
-//     // this.acc = createVector(random(-1,1),random(-1,1));
-//
-//     // method #2
-//     // this.acc = p5.Vector.fromAngle(3*PI/2);
-//     // this.acc = p5.Vector.fromAngle(random(TWO_PI));
-//
-//     // method 3
-//     // fixed acceleration in object properties, rotate in update function
-//
-//     this.acc.rotate(0.01);
-// // Magnitude
-// // Method #1
-//     // normalize vector to be of length one
-//     // still in the direction of target location
-//     // this.acc.normalize();
-//
-//     // shrink acceleration vector
-//     // this.acc.mult(0.01);
-// // Method #2
-//     this.acc.setMag(0.18);
-//
-//     // acceleration changes velocity
-//     this.vel.add(this.acc);
-//     // velocity changes position
-//     this.pos.add(this.vel);
-
-
-
-
-    // this.pos.x = this.pos.x + random(-1, 1);
-    // this.pos.y = this.pos.y + random(-1, 1);
-
-  // this.update = function() {
-  //
-  // }
-  this.display = function() {
+        // for (var j = cells.length - 1; j >= 0; j--) {
+        //     if (j != i && cells[i] && cells[i].eats(cells[j])) {
+        //         cells.splice(j, 1);
+        //     }
+        // }
+    }
+    textSize(24);
     fill(255);
-    ellipse(this.pos.x, this.pos.y, 48, 48);
-  }
+    text("# of Cells in System:", 10, 30);
+    text(str(cells.length), 10, 60);
 }
 
 
 // References
 // https://forum.processing.org/one/topic/create-new-object-with-mouse-click.html
+// https://forum.processing.org/two/discussion/24872/#Comment_109265
