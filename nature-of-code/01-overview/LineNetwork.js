e0f6eb799aa07fed49785ceb3c9e5760d6fc73ba
christian <christianlemp@gmail.com>
2018-05-15 22:20:56 -0400

    lines to each EndCircle pos

--- nature-of-code/01-overview/LineNetwork.js
+++ nature-of-code/01-overview/LineNetwork.js
@@ -1,13 +1,21 @@
+// Meant to have passed one OriginCircle, one EndCircle object
+// for x1,y1,x2,y2 coordinates.
+line_network = []
+
 class LineNetwork {
-  constructor(x1, y1, x2, y2) {
-    this.pos_start = createVector(x1, y1);
-    this.pos_end = createVector(x2, y2);
+  constructor(obj) {
+    this.pos_start = createVector(obj1.pos.x, obj1.pos.y);
+    this.pos_end = createVector(obj2.pos.x, obj2.pos.y);
   }
 
   run() {
     this.display();
   }
 
+  add_line() {
+    stroke(255);
+    line()
+  }
   // Method to display
   display() {
     stroke(255);
--- nature-of-code/01-overview/motion-networks.js
+++ nature-of-code/01-overview/motion-networks.js
@@ -1,6 +1,6 @@
-var objects = [];
-var lines = [];
+var origins = [];
 var ends = [];
+var lines = [];
 
 // Set up array to store x, y coordinates of previous object origin
 var prev_pos = [];
@@ -23,55 +23,75 @@ function mouseClicked() {
     // Store position vector
     prev_pos.push(pos);
     print("---- mousePressed() -----")
-    print("new object x: ", str(pos.x), "new object y: ", str(pos.y));
-    print("objects length: ", str(objects.length));
+    print("new origin x: ", str(pos.x), "new origin y: ", str(pos.y));
+    print("objects length: ", str(origins.length));
 
     print("new line x1: ", str(pos.x), "new line y1: ", str(pos.y));
     print("new line x2: ", str(pos.x), "new line y2: ", str(pos.y));
-    print("lines length: ", str(objects.length));
+    // print("lines length: ", str(objects.length));
 
-    print("prev_pos length: ", str(prev_pos.length))
-    if (prev_pos.length > 1) {
-      print("prev_pos x: ", str(prev_pos[prev_pos.length-2].x), "prev_pos y: ", str(prev_pos[prev_pos.length-2].y));
-    }
-
-
-
+    // print("prev_pos length: ", str(prev_pos.length))
+    // if (prev_pos.length > 1) {
+    //   print("prev_pos x: ", str(prev_pos[prev_pos.length-2].x), "prev_pos y: ", str(prev_pos[prev_pos.length-2].y));
+    // }
 
     // Create new OriginCircle object, and add it to the 'objects' array
     origin_radius = random(65,95);
-    end_radius = 15;
+    end_radius = 25;
 
-    objects.push(new OriginCircle(pos.x, pos.y, origin_radius));
+    origins.push(new OriginCircle(pos.x, pos.y, origin_radius));
 
-    for (var i = 700; i >= 0; i--) {
-      objects.push(new EndCircle(pos.x, pos.y, end_radius));
+    for (var i = 1; i >= 0; i--) {
+      // distance away from origin is regulated inside the EndCircle object
+      o = new EndCircle(pos.x, pos.y, end_radius);
+      ends.push(o);
     }
-
 
     // Creates new LineNetwork object, adds to the 'lines' array
     // If this is the first time the mouse is being pressed, line is created
     // as a single dot
-
-    if (lines.length > 1) {
-      lines.push(new LineNetwork(pos.x, pos.y, prev_pos[prev_pos.length-2].x, prev_pos[prev_pos.length-2].y));
-    } else {
-      lines.push(new LineNetwork(pos.x, pos.y, pos.x, pos.y));
-    }
+    // lines.push(new LineNetwork(pos.x, pos.y, prev_pos[prev_pos.length-2].x, prev_pos[prev_pos.length-2].y));
 
     // } else {
     //   lines.push(new LineNetwork(pos.x, pos.y, prev_pos[0].x, prev_pos[0].y));
     // }
   }
 
+function line_network(ends) {
+  // Draw line
+  if (ends.length > 1) {
+    for (var i = ends.length-1; i >= 1; i--) {
+      stroke(255);
+      line(ends[i].pos.x, ends[i].pos.y, 0, 0);
+      stroke(255);
+      line(ends[i-1].pos.x, ends[i-1].pos.y, width, height);
+    }
+  }
+}
+
 function draw() {
   background(51);
-  // Run every OpenCircle object in the 'objects' array
-  for (var i = objects.length - 1; i >= 0; i--) {
-    objects[i].run();
+
+  // Run every OriginCircle object in the 'origins' array
+  for (var i = origins.length - 1; i >= 0; i--) {
+    origins[i].run();
+  }
+  // Run every EndCircle object in the 'ends' array
+  for (var i = ends.length - 1; i >= 0; i--) {
+    ends[i].run();
   }
 
-  // Run every LineNetwork object in the 'lines' array
+  line_network(ends);
+
+
+// i = 1; ends.length = 1;
+//
+//
+//
+//
+//
+
+    // Run every LineNetwork object in the 'lines' array
   // for (var i = lines.length - 1; i >= 0; i--) {
     // lines[i].run();
   // }
