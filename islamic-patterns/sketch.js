65c6640a0920ae7df4555b4aca8bf7562da9db37
christian <christianlemp@gmail.com>
2018-07-21 17:52:48 -0400

    added grid

---
+++ islamic-patterns/sketch.js
@@ -0,0 +1,43 @@
+// var poly;
+var polys = [];
+// var angle = 60;
+var angle = 75;
+var delta =2;
+
+var angleSlider;
+var deltaSlider;
+
+function setup() {
+  createCanvas(600,600);
+  deltaSlider = createSlider(1,25,10);
+  angleSlider = createSlider(0,90,60);
+
+  angleMode(DEGREES);
+  var inc = 100;
+  for (var x = 0; x < width; x+=inc) {
+    for (var y = 0; y < height; y += inc) {
+      var poly = new Polygon(x, y);
+      poly.addVertex(x, y);
+      poly.addVertex(x + inc, y);
+      poly.addVertex(x + inc, y + inc);
+      poly.addVertex(x, y + inc);
+      poly.close();
+      polys.push(poly);
+    }
+  }
+
+}
+
+function draw() {
+  background(51);
+
+  delta = deltaSlider.value();
+  angle = angleSlider.value();
+
+  for (var i = 0; i < polys.length; i++) {
+    polys[i].hankin();
+    polys[i].show();
+  }
+  // poly.hankin();
+  // poly.show();
+}
